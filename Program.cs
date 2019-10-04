6b1d487c186347eaf5c8d9e845f6352b51fc6eaa
Brad Linard <brad.linard@idelata.com>
2019-10-03 20:03:48 -0400

    Initial actual commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TextCopy;
+
+namespace GimmieAGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newGuid = Guid.NewGuid().ToString();
+            Clipboard.SetText(newGuid);
+            Console.WriteLine(newGuid);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gimmie-a-guid
 A simple command line tool to generate a guid. Because, sometimes, you just need a guid.
+
+
+Credit: https://github.com/SimonCropp/TextCopy
